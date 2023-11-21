6fc94f312786efc07b6faccf265ef9d0d3894f71
akshatvermavi <akshatvermanike@gmail.com>
2023-11-21 07:55:13 +0530

    about feature is updated

--- app/views.py
+++ app/views.py
@@ -10,3 +10,5 @@ def home(request):
     return render(request,'home.html')
 def about(request):
     return render(request,'about.html')
+# def about(request):
+#     return render(request,'create_event.html')
